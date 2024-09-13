933f80ed176693a6174300f7cbd4ee0a0d5ba9d6
Olle Ljung <olle.ljung@gmail.com>
2024-09-13 15:52:20 +0200

    Log clients during triggering popup

--- src/lib/api/ws.ts
+++ src/lib/api/ws.ts
@@ -1,6 +1,7 @@
 import { Popup } from "@/db/schema"
 
 import { WebSocket } from "ws"
+import { serverLogger } from "../utils/server/logging"
 
 export const clients: Map<string, Set<WebSocket>> = new Map()
 
@@ -9,6 +10,7 @@ export function triggerPopup(siteId: string, popup: Popup) {
   if (siteClients) {
     const message = JSON.stringify({ type: "show_popup", popup })
     for (const client of siteClients) {
+      serverLogger.info({ type: "WS", msg: "Sending popup to client", details: { siteId, client } })
       if (client.readyState === WebSocket.OPEN) {
         client.send(message)
       }
