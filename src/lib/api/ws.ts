b0ee8ea10ed433ee0b59d5e25af0473ee38bc4b8
Olle Ljung <olle.ljung@gmail.com>
2024-09-13 18:52:39 +0200

    More loggin again

--- src/lib/api/ws.ts
+++ src/lib/api/ws.ts
@@ -6,16 +6,21 @@ import { serverLogger } from "../utils/server/logging"
 export const clients: Map<string, Set<WebSocket>> = new Map()
 
 export function triggerPopup(siteId: string, popup: Popup) {
+  serverLogger.info({ type: "WS", msg: "Attempting to trigger popup", details: { siteId } })
   const siteClients = clients.get(siteId)
   if (siteClients) {
-    serverLogger.info({ type: "WS", msg: "Found Clients - Triggering popup", details: { siteId, popup } })
+    serverLogger.info({ type: "WS", msg: "Found Clients - Triggering popup", details: { siteId, clientCount: siteClients.size } })
     const message = JSON.stringify({ type: "show_popup", popup })
+    let sentCount = 0
     for (const client of siteClients) {
       if (client.readyState === WebSocket.OPEN) {
         client.send(message)
+        sentCount++
       }
     }
+    serverLogger.info({ type: "WS", msg: "Popup triggered", details: { siteId, sentCount } })
   } else {
     serverLogger.info({ type: "WS", msg: "No clients found for site", details: { siteId } })
   }
+  serverLogger.info({ type: "WS", msg: "Current Map state", details: { mapSize: clients.size, allSiteIds: Array.from(clients.keys()) } })
 }
