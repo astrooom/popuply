d0ac52a7bbfd05f2dcd3335d44ea4ab18a8ef815
Olle Ljung <olle.ljung@gmail.com>
2024-09-13 20:42:17 +0200

    Adjusted price ids for live mode

--- src/lib/constants/checkout.ts
+++ src/lib/constants/checkout.ts
@@ -1,7 +1,7 @@
 export const NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
 
 export const PRODUCTS = [
-  { id: "1site", name: "1 Site", price: 10, sites: 1, pricingId: "price_1PxskRCbIUymC3Vfavu7QhU9" },
-  { id: "5sites", name: "5 Sites", price: 20, sites: 5, pricingId: "price_1Pxsl9CbIUymC3Vf8riz4rDZ" },
-  { id: "10sites", name: "10 Sites", price: 30, sites: 10, pricingId: "price_1PxzcyCbIUymC3VfGckzhA55" },
+  { id: "1site", name: "1 Site", price: 10, sites: 1, pricingId: "price_1PyeQ8CbIUymC3VfSieWKVCt" },
+  { id: "5sites", name: "5 Sites", price: 20, sites: 5, pricingId: "price_1PyePnCbIUymC3VfZS69M7Cs" },
+  { id: "10sites", name: "10 Sites", price: 30, sites: 10, pricingId: "price_1PyeQVCbIUymC3VfqdObNV7v" },
 ]
